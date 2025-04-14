70a49fd90e00c9ca6df035917ed5d0cfcf18d4a6
dong881 <minuhunghsu.taiwan@gmail.com>
2025-04-14 11:47:02 +0800

    Refactor run_gNB.sh: rename CMD_VNF to CMD_VNF_100M, add CMD_VNF_40MHz and CMD_Monolithic_40MHz for 40MHz support

--- run_gNB.sh
+++ run_gNB.sh
@@ -5,9 +5,13 @@ USER="oai72"
 HOST="192.168.8.43"
 PASSWORD="bmwlab"
 
-CMD_VNF="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S NFAPI_TRACE_LEVEL=info ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb-vnf.sa.band78.273prb.nfapi.conf --nfapi VNF"
+CMD_VNF_100M="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S NFAPI_TRACE_LEVEL=info ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb-vnf.sa.band78.273prb.nfapi.conf --nfapi VNF"
 CMD_PNF="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S NFAPI_TRACE_LEVEL=info ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb-pnf.band78.fhi72.4x4-liteon_new.conf --nfapi PNF --reorder-thread-disable 1 --thread-pool 1,3,5,7,9,11,13,15"
-CMD_Monolithic="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.273prb.fhi72.4x4-liteon_new.conf --thread-pool 1,3,5,7,9,11,13,15 "
+CMD_Monolithic="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.273prb.fhi72.4x4-liteon_new.conf --thread-pool 1,3,5,7,9,11,13,15"
+
+# Commands for 40MHz (106PRB)
+CMD_VNF_40MHz="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S NFAPI_TRACE_LEVEL=info ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb-vnf.sa.band78.106prb.nfapi.conf --nfapi VNF"
+CMD_Monolithic_40MHz="cd ~/FH_7.2_dev/openairinterface5g/cmake_targets/ran_build/build && echo '$PASSWORD' | sudo -S ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.106prb.fhi72.4x4-liteon_new --thread-pool 1,3,5,7,9,11,13,15"
 
 # Function to start a screen session
 start_session() {
@@ -23,11 +27,19 @@ stop_session() {
 }
 
 # Start sessions
-# start_session "VNF" "$CMD_VNF"
+start_session "VNF" "$CMD_VNF_100M"
 # start_session "PNF" "$CMD_PNF"
-start_session "Monolithic" "$CMD_Monolithic"
+# start_session "Monolithic" "$CMD_Monolithic"
+
+# Start sessions for 40MHz
+# start_session "VNF_40MHz" "$CMD_VNF_40MHz"
+start_session "PNF" "$CMD_PNF"
+# start_session "Monolithic_40MHz" "$CMD_Monolithic_40MHz"
 
 # Example to stop sessions (uncomment to use)
 # stop_session "VNF"
 # stop_session "PNF"
 # stop_session "Monolithic"
+# stop_session "VNF_40MHz"
+# stop_session "PNF"
+# stop_session "Monolithic_40MHz"
