703697cd21cd49783d72b5dd1fc6d81393008727
dong881 <minghunghsu.taiwan@gmail.com%>
2025-05-28 02:03:40 +0800

    20250528-nFAPI(100-500M)

--- tools/clean_iperf_json.py
+++ tools/clean_iperf_json.py
@@ -2,7 +2,7 @@ import os
 import glob
 import json
 
-DATA_DIR = "/home/ming/E2E-network-measurement/data/20250528-nFAPI(100-400M)"
+DATA_DIR = "/home/ming/E2E-network-measurement/data/20250528-nFAPI(100-500M)"
 
 def is_error_block(obj):
     return (
